b1da516fbf7330e4860a22a67559adcb6fa98ff5
TrevourGuild <trevorr.guildd@gmail.com>
2021-07-16 10:50:12 -0700

    added names data to dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
+import Character from './components/Character'
+import axios from 'axios'
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +12,40 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+
+  const [characters, setCharacters] = useState([])
+  const [currentCharIndex, setCurrentCharIndex] = useState(null)
+
+  const openDetails = index => {
+    setCurrentCharIndex(index)
+  }
+
+  const closeDetails = () => {
+    setCurrentCharIndex(null)
+  }
+
+
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people/`)
+      .then(res => {
+        setCharacters(res.data)
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      {
+        characters.map(ch =>{
+          return <Character key={ch.name} info={ch} action={openDetails}/>
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from 'react'
+
+
+export default function Character({ info, action }){
+    return (
+        <div className = 'character'>
+            {info.name}
+            <button onClick = {() => action(info.name)}>
+            ^
+            </button>
+        </div>
+    )
+}
